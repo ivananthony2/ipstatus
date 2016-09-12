a34252ac4de53ad4891cacdccd3d74fa1550ab24
Isaiah Vanderpas <isaiahvanderpas587@gmail.com>
2016-09-12 10:16:43 -0500

    removed comparator added login fail exception

--- ipRequire.rb
+++ ipRequire.rb
@@ -10,20 +10,24 @@ loop do
 
   if old_ip != remote_ip
     Gmail.connect(ENV["EMAIL"], ENV["PASSWORD"]) do |g|
-      if g.logged_in? == true
+      if g.logged_in?
         puts 'Gmail Logged ON'
+      else
+      	puts 'Something Happened'
+      break
       end
 
       g.deliver do
-        to "isaiahavanderpas@gmail.com"
-        subject "Your Remote IP Address Has Been Changed"
+        to ENV["EMAIL"]
+        subject 'Your Remote IP Address Has Been Changed'
         text_part do
-          body "Your old address was #{old_ip}. Your new address is #{remote_ip}"
+          body 'Your old address was #{old_ip}. Your new address is #{remote_ip}'
         end
       end
     end
 
     old_ip = remote_ip 
+    
   end
 
   sleep(10)
