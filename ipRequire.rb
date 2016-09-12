5ff0f0036a4021a20e69101b4f70bfea2ad0d7fb
Jeremiah Piontek <jwpiontek@gmail.com>
2016-09-12 09:40:09 -0500

    cleanup

--- ipRequire.rb
+++ ipRequire.rb
@@ -3,34 +3,27 @@ require 'open-uri'
 
 old_ip = ""
 
-#loop do
+loop do
 
-remote_ip = open('http://whatismyip.akamai.com').read
+  remote_ip = open('http://whatismyip.akamai.com').read
 
-new_ip = remote_ip
-	
-		if old_ip != remote_ip
-	
+  if old_ip != remote_ip
+    Gmail.connect('isaiahavanderpas@gmail.com', ENV["PASSWORD"]) do |g|
+      if g.logged_in? == true
+        puts 'Gmail Logged ON'
+      end
 
-	gmail = Gmail.connect('isaiahavanderpas@gmail.com', ENV["PASSWORD"]) do |gmail|
-	 if gmail.logged_in? == true
-	 	puts 'Gmail Logged ON'
-	 end
+      g.deliver do
+        to "isaiahavanderpas@gmail.com"
+        subject "Your Remote IP Address Has Been Changed"
+        text_part do
+          body "Your old address was #{old_ip}. Your new address is #{new_ip}"
+        end
+        old_ip = remote_ip 
+      end
+    end
+  end
 
-
-gmail.deliver 	do
-  to "isaiahavanderpas@gmail.com"
-  subject "Your Remote IP Address Has Been Changed"
-  text_part do
-    body "Your old address was #{old_ip}. Your new address is #{new_ip}"
-  			end
-  				end
-old_ip = new_ip
-
-  	sleep(3)
-
-		
-#end
+  sleep(10)
 
 end
-	end
